1acfb543f2711f4b9736cd870e087a07379c36f8
Daniel <oralordos@gmail.com>
2016-01-14 10:30:07 -0800

    Added surface normal calculations

--- ellipsoid.go
+++ ellipsoid.go
@@ -1,6 +1,10 @@
 package globe
 
-import "github.com/ungerik/go3d/float64/vec3"
+import (
+	"math"
+
+	"github.com/ungerik/go3d/float64/vec3"
+)
 
 // Ellipsoid holds the necessary information to process a 3d ellipsoidal shape.
 type Ellipsoid struct {
@@ -17,7 +21,7 @@ func NewEllipsoid(x, y, z float64) *Ellipsoid {
 
 // NewEllipsoidVec creates a new ellipsoid with the radii equaling the x, y, and z values of the given vector.
 func NewEllipsoidVec(radii *vec3.T) *Ellipsoid {
-	if radii.Get(0, 0) < 0 || radii.Get(0, 1) < 0 || radii.Get(0, 2) < 0 {
+	if radii[0] < 0 || radii[1] < 0 || radii[2] < 0 {
 		panic("Ellipsoid with smaller than 0 radii.")
 	}
 
@@ -26,7 +30,7 @@ func NewEllipsoidVec(radii *vec3.T) *Ellipsoid {
 		radii:               *radii,
 		radiiSquared:        *sqr,
 		radiiToTheFourth:    *sqr.Mul(sqr),
-		oneOverRadiiSquared: vec3.T{1 / sqr.Get(0, 0), 1 / sqr.Get(0, 1), 1 / sqr.Get(0, 2)},
+		oneOverRadiiSquared: vec3.T{1 / sqr[0], 1 / sqr[1], 1 / sqr[2]},
 	}
 }
 
@@ -43,4 +47,17 @@ func (e *Ellipsoid) GetRadiiSquared() *vec3.T {
 // GetOneOverRadiiSquared returns the radii vector of the ellipsoid where each radii has been squared and then the reciprocal is found.
 func (e *Ellipsoid) GetOneOverRadiiSquared() *vec3.T {
 	return &e.oneOverRadiiSquared
+}
+
+// GeodeticSurfaceNormalCart returns the proper surface normal for a point in cartesian coordinates for the ellipsoid.
+func (e *Ellipsoid) GeodeticSurfaceNormalCart(posOnEllipsoid *vec3.T) *vec3.T {
+	return posOnEllipsoid.Mul(&e.oneOverRadiiSquared).Normalize()
+}
+
+// GeodeticSurfaceNormalGeo returns the proper surface normal for a point in geodetic coordinates for the ellipsoid.
+func (e *Ellipsoid) GeodeticSurfaceNormalGeo(posOnEllipsoid *Geo3D) *vec3.T {
+	lat, lon := posOnEllipsoid.LatLon()
+	cosLat := math.Cos(lat)
+
+	return &vec3.T{cosLat * math.Cos(lon), cosLat * math.Sin(lon), math.Sin(lat)}
 }
